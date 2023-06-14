8506158c8d00efa3a99425cdd689d245df44504b
Goliothjoe <s11910279@student.unklab.ac.id>
2023-06-14 20:45:38 +0800

    Golioth, Joe

---
+++ pertemuan4/index.js
@@ -0,0 +1,63 @@
+//Variabel 
+
+// let firstName ;
+// firstName = "John Doe";  //string
+// let age = "18" //Number
+// let isMarried = true; //Boolean
+
+// console.log(firstName);
+// console.log(age);
+// console.log(isMarried);
+// console.log("Hallo nama saya" + firstName + "umur saya" + age + "saya belum menikah" + isMarried);
+
+// const gender = "male";
+
+// let x = 10
+// let y = 20 
+
+// y = x
+// //x = 20, y = 10
+
+
+// let x = 20
+// let y = 20
+
+// console.log(x==y); //true
+// console.log(x===y); //false
+
+//ternary operator
+// let age = 17;
+// let result = age >= 17 ? "Bisa buat KTP" : "Belum bisa buat KTP";
+// console.log(result); 
+
+//function
+
+// function greetings() {
+//     console.log("Hello world");
+
+// }
+   
+// const greetings2 = function () {
+//     console.log("Hello world 2");
+// }
+
+// greetings();
+// greetings2();
+
+
+let firstName = "John Doe";
+
+function greetings() {
+    let age = 30; //local function
+    console.log(firstName);
+    if(age === 30) {
+        let isEligible = true; //Local blok
+    }
+    console.log(isEligible);
+}
+
+greetings(); 
+
+
+
+
