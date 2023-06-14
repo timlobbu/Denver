29e9bcf2a29446ec3e3cf4a31b5c07a1bff57dad
timlobbu <timlobbu@gmail.com>
2023-06-14 09:39:14 +0800

    adding files

---
+++ index.js
@@ -0,0 +1,79 @@
+// // //Array
+
+// // let students = ["john", "jane", "bob", "peter"];
+// // let john = ["john", "doe", 33, true];
+
+// // // console.log(students.length);
+
+// // // students[2] = "edgar";
+
+// // // console.log(students);
+// // // console.log(john);
+
+// // let array = [1, 2, 3, "halo", false, true];
+// // // console.log(array.toString()); //array toString
+// // // console.log(array.join("-")); //array join
+// // array.pop(); //menghilangkan yang paling akhir
+// // console.log(array);
+// // array.push("selamat pagi");//menambahkan yang paling akhir
+// // console.log(array);
+// // array.shift();//menghilangkan yang paling pertama
+// // console.log(array);
+// // array.unshift("Hallo");//menambahkan yang paling pertama
+// // console.log(array);
+
+// // let buah = ["Pisang", "Jeruk", "Apel", "Mangga"];
+// // buah.splice(1, 0, "Lemon", "Kiwi");
+// // console.log(buah);
+
+// // //Object
+
+// // let johnObj = {
+// //     firstName: "John",
+// //     lastName: "Doe",
+// //     age: 33,
+// //     isMarried: true,
+// // };
+
+// // // console.log(johnObj.firstName);
+// // // console.log(johnObj.lastName);
+// // // console.log(johnObj["age"]);
+
+// // // for loop
+
+// // for (let i = 1; i <= 10; i++) {
+// //     console.log(i);
+// //   }
+
+
+// //Array Built in Method
+
+// let students = [
+//     {
+//         fullName: "John Doe",
+//         age: 33,
+//         grade: 90,
+//     },
+//     {
+//         fullName: "Peter",
+//         age: 31,
+//         grade: 80,
+//     },
+//     {
+//         fullName: "Jane",
+//         age: 36,
+//         grade: 100,
+//     },
+// ];
+
+// // for (let i = 0; i < students.length; i++){
+// //     students[i].grade -= 5;
+// //     console.log(students[i]);
+// // }
+
+// students.forEach(function(item){
+//     item.grade -= 10;
+//     console.log (item);
+// });
+
+
